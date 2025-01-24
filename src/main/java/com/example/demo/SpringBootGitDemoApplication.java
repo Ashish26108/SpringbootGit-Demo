df0725516f334e9e4d8fa055dd8ee0ce8d123584
ashish Bansal <ashish26108@gmail.com>
2025-01-24 14:36:01 +0530

    first changes commit

--- src/main/java/com/example/demo/SpringBootGitDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootGitDemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootGitDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGitDemoApplication.class, args);
+		ConfigurableApplicationContext run=SpringApplication.run(SpringBootGitDemoApplication.class, args);
+		run.close();
 	}
 
 }
