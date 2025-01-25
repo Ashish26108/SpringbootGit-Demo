5c66a61701637651e1de36aa3fc7aabb74eeb2ce
ashish Bansal <ashish26108@gmail.com>
2025-01-25 11:23:36 +0530

    His-301 related changes

--- src/main/java/com/example/demo/SpringBootGitDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootGitDemoApplication.java
@@ -6,11 +6,18 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootGitDemoApplication {
+	
+	private Map<String,Object> cache= new HashMap<String,Object>();
+	 
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(SpringBootGitDemoApplication.class, args);
 		run.close();
 		//Changes for HIS-134
+	}
+	
+	public void loadDataToCache() {
+		
 	}
 
 }
